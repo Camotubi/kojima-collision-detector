baecbf85bea1c0887b6fe3ac310251612c93d944
Camotubi <camotubi@gmail.com>
2018-05-10 20:21:12 -0500

    Add collision detector class

---
+++ app/src/main/java/com/ecs/collisiondetector/DistanceCalculator.kt
@@ -0,0 +1,24 @@
+package com.ecs.collisiondetector
+class DistanceCalculator {
+
+    var focalLength:Double = 0.0
+
+
+  fun calibrateFocalLength(pixelWidth:Double, width:Double, distance:Double) {
+    this.focalLength = calculateFocalLength(pixelWidth, width, distance)
+  }
+
+  fun calculateDistance(pixelWidth:Double, width:Double):Double {
+    return width * this.focalLength / pixelWidth
+  }
+
+  companion object {
+    fun calculateFocalLength(pixelWidth:Double, width:Double, distance:Double):Double {
+      return pixelWidth * distance / width
+    }
+    fun calculateDistance(pixelWidth:Double, width:Double, focalLength:Double):Double {
+      return width * focalLength / pixelWidth
+    }
+  }
+  
+}
--- app/src/main/java/com/ecs/collisiondetector/MainActivity.kt
+++ app/src/main/java/com/ecs/collisiondetector/MainActivity.kt
@@ -1,12 +1,25 @@
 package com.ecs.collisiondetector
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.View.OnClickListener
+import android.widget.Button
+import com.ecs.collisiondetector.yolo2.view.ClassifierActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() , OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val button : Button = findViewById(R.id.button)
+        button.setOnClickListener(this)
+
+    }
+
+    override fun onClick(v: View?) {
+        val intent = Intent(this, ClassifierActivity::class.java)
+        this.startActivity(intent)
     }
 }
