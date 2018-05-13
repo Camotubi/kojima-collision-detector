5d42dfd9eedb21d1ef23c9f0d03b1dfd889e3f8c
Camotubi <camotubi@gmail.com>
2018-05-13 04:59:09 -0500

    add edge detection class

---
+++ app/src/main/java/com/ecs/collisiondetector/EdgeDetection/EdgeDetector.kt
@@ -0,0 +1,41 @@
+package com.ecs.collisiondetector.EdgeDetection
+
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+
+class EdgeDetector {
+    var bitmap :Bitmap = Bitmap.createBitmap(1,1,Bitmap.Config.RGB_565)
+    fun EdgeDetector(bmp: Bitmap)
+    {
+        bitmap = bmp
+    }
+    companion object {
+        fun getFarthest(bmp : Bitmap) : Array<IntArray> {
+            var p1 = intArrayOf(-1,-1)
+            var p2 = intArrayOf(-1,-1)
+            for(y in 0..(bmp.height-1)) {
+                for (x in 0..(bmp.width-1)) {
+                    val pix = bmp.getPixel(x,y)
+                    if(pix != -16777216) {
+                        if(p1[0]==-1) {
+                            p1[0] = x
+                            p1[1] = y
+                        } else {
+                            p2[0] = x
+                            p2[1] = y
+                        }
+                    }
+                }
+            }
+            return arrayOf(p1,p2)
+        }
+        fun getWidth(bmp: Bitmap) : Int{
+            val farthests = getFarthest(bmp)
+            return Math.abs(farthests[0][0] - farthests[1][0])
+        }
+        fun getHeigth(bmp: Bitmap) : Int{
+            val farthests = getFarthest(bmp)
+            return Math.abs(farthests[0][1] - farthests[1][1])
+        }
+    }
+}
--- app/src/main/java/com/ecs/collisiondetector/MainActivity.kt
+++ app/src/main/java/com/ecs/collisiondetector/MainActivity.kt
@@ -15,6 +15,7 @@ import android.graphics.drawable.BitmapDrawable
 import android.util.Log
 import android.widget.ImageView
 import com.ecs.collisiondetector.EdgeDetection.Canny
+import com.ecs.collisiondetector.EdgeDetection.EdgeDetector
 import org.opencv.android.Utils
 import org.opencv.imgproc.Imgproc
 import org.opencv.core.CvType
@@ -23,11 +24,7 @@ import org.opencv.core.Mat
 import org.opencv.android.LoaderCallbackInterface
 import org.opencv.android.OpenCVLoader
 import org.opencv.android.BaseLoaderCallback
-
-
-
-
-
+import java.nio.ByteBuffer
 
 
 class MainActivity : AppCompatActivity() , OnClickListener {
@@ -71,8 +68,11 @@ class MainActivity : AppCompatActivity() , OnClickListener {
             Log.d("OpenCV", "OpenCV library found inside package. Using it!");
             mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
         }
-        val bmp = BitmapFactory.decodeResource(resources,R.drawable.box)
+
+        val bmp = BitmapFactory.decodeResource(resources,R.drawable.wot)
         val edgeBmp = Canny.detectEdges(bmp)
+        val edgeWidth = EdgeDetector.getWidth(edgeBmp)
+        Log.d("WIDTH APARATO", "Width: $edgeWidth")
         val edgeDrawable = BitmapDrawable(resources,edgeBmp)
         val boxDrawable = BitmapDrawable(resources,bmp)
         val imgView = findViewById<ImageView>(R.id.imageView2)
